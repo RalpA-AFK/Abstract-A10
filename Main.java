fc3bc01dd94ed2f2cc153ee7eae96161d7ee0514
RalpA-AFK <ralpaa@farmingdale.edu>
2025-04-20 21:14:54 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Archer p1 = new Archer("Sally", "Razorpine");
+		
+		p1.Dialouge1(p1.getName());
+		p1.Dialouge2(p1.getWeaponName());
+
+	}
+
+}
